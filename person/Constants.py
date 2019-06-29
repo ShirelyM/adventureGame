9368bab6caf83095f0b94abbd03734f6cafcfb51
justinrgibbs <nano.fsu@gmail.com>
2019-06-29 17:42:56 -0400

    adding constants

---
+++ person/Constants.py
@@ -0,0 +1,19 @@
+class HairColor:
+    BLONDE = 0
+    BROWN = 1
+    BLACK = 2
+    RED = 3
+
+
+class EyeColor:
+    BLUE = 0
+    BROWN = 1
+    GREEN = 2
+    HAZEL = 3
+
+
+class Gender:
+    MALE = 0
+    FEMALE = 1
+    X = 3
+
