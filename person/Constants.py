36d89efdc498e08dc13389f23696cbc1eb9e56e6
Michael <mds10g@my.fsu.edu>
2019-06-29 17:44:56 -0400

    Cnstants

--- person/Constants.py
+++ person/Constants.py
@@ -1,19 +1,19 @@
 class HairColor:
-    BLONDE = 0
-    BROWN = 1
-    BLACK = 2
-    RED = 3
+    BLONDE  = 0
+    BROWN   = 1
+    BLACK   = 2
+    RED     = 3
 
 
 class EyeColor:
-    BLUE = 0
-    BROWN = 1
-    GREEN = 2
-    HAZEL = 3
+    BLUE    = 0
+    BROWN   = 1
+    GREEN   = 2
+    HAZEL   = 3
 
 
 class Gender:
-    MALE = 0
-    FEMALE = 1
-    X = 3
+    MALE    = 0
+    FEMALE  = 1
+    OTHER   = 2
 
